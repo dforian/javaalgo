d436a4cf782fa3668219b1dfae6a255e7ec9b6b4
dforian <68153509+dforian@users.noreply.github.com>
2021-04-29 13:23:34 +0300

    algo project

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,18 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        int sum = 0;
+        if(input == null || input.isEmpty()) {
+            return -1;
+        }
+        else {
+            for(int i = 0; i < input.length(); i++) {
+                if(input.charAt(i) >= '1' && input.charAt(i) <= '9')
+                sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+            }
+        }
+        return sum;
+    }
 }
